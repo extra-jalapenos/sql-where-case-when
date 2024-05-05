7cc6c24b1795c8392dadc8bc39dcb8be8d943a18
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:37:16 +0200

    change indentation type

--- standard_criteria/6_solution.sql
+++ standard_criteria/6_solution.sql
@@ -2,8 +2,8 @@
 -- General Pop: female 10771, male 21790
 
 SELECT
-    sex,
-    ROUND(COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) * 100.0 / COUNT(*), 2) AS doctorates_percentage
+	sex,
+	ROUND(COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) * 100.0 / COUNT(*), 2) AS doctorates_percentage
 FROM
-    `bigquery-public-data.ml_datasets.census_adult_income`
+	`bigquery-public-data.ml_datasets.census_adult_income`
 GROUP BY sex;
