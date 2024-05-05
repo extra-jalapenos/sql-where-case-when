fb36e1ca81949976f67ec4e18a3556a4a93b2dc3
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 11:01:23 +0200

    add query 1

---
+++ standard_criteria/1_solution.sql
@@ -0,0 +1,10 @@
+SELECT age,
+  CASE
+    WHEN age < 20 THEN "<20"
+    WHEN (age >= 20 AND age <= 39) THEN "20-39"
+    WHEN (age >= 40 AND age <= 59) THEN "40-59"
+    WHEN (age >= 60 AND age <= 79) THEN "60-79"
+    WHEN (age >= 80) THEN ">=80"
+    ELSE "unknown"
+  END as age_bracket
+FROM `bigquery-public-data.ml_datasets.census_adult_income` LIMIT 1000
