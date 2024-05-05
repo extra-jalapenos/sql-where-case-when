5abcd7ed61ee4399b95626041abc7d0aaf239c84
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:14:32 +0200

    add advanced criteria 1

---
+++ extension_criteria/1_query.sql
@@ -0,0 +1,9 @@
+SELECT
+    TRIM(sex) as sex,
+    COUNT(*) AS total_population,
+    COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) AS doctorates,
+    ROUND(COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) * 100.0 / COUNT(*), 2) AS doctorates_percentage
+FROM
+    `bigquery-public-data.ml_datasets.census_adult_income`
+GROUP BY
+    sex;
