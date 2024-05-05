3a1d7571f718546c0fbe92d9ef1ce78c371b5078
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:25:47 +0200

    change formatting

--- standard_criteria/6_solution.sql
+++ standard_criteria/6_solution.sql
@@ -6,5 +6,4 @@ SELECT
     ROUND(COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) * 100.0 / COUNT(*), 2) AS doctorates_percentage
 FROM
     `bigquery-public-data.ml_datasets.census_adult_income`
-GROUP BY
-    sex;
+GROUP BY sex;
