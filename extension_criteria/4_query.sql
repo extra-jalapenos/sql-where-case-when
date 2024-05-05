fe7c40233906a6cd9c4d963f165febdee8cac99a
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:56:19 +0200

    change whitespace type

--- extension_criteria/4_query.sql
+++ extension_criteria/4_query.sql
@@ -1,7 +1,12 @@
 SELECT
 	SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END) AS Male_Doctorates_Percent,
 	SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END) AS Female_Doctorates_Percent,
-	ROUND((SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END))/(SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END)), 2) AS FemMalDocRatio
+	CASE
+		WHEN SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END) = 0 THEN null
+		ELSE
+			ROUND((SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END))/(SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END)), 2)
+		END
+	AS FemMalDocRatio
 FROM
 	`bigquery-public-data.ml_datasets.census_adult_income`
 WHERE TRIM(native_country) = "Italy"
