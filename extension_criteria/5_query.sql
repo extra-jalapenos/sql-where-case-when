d5ccaa721c02bc0cc518951641836552bc8091e2
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:56:44 +0200

    change whitespace type

--- extension_criteria/5_query.sql
+++ extension_criteria/5_query.sql
@@ -3,11 +3,11 @@
 -- let's compare it by country
 
 SELECT
-    TRIM(native_country) as country,
-    SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END), 0) AS Male_Doctorates_Percent,
-    SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END), 0) AS Female_Doctorates_Percent,
-    ROUND((SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END), 0)) / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END), 0), 0), 2) AS Male_To_Female
+	TRIM(native_country) as country,
+	SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END), 0) AS Male_Doctorates_Percent,
+	SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END), 0) AS Female_Doctorates_Percent,
+	ROUND((SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END), 0)) / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END), 0), 0), 2) AS Male_To_Female
 FROM
-    `bigquery-public-data.ml_datasets.census_adult_income`
+	`bigquery-public-data.ml_datasets.census_adult_income`
 GROUP BY country
 ORDER BY Male_To_Female DESC
