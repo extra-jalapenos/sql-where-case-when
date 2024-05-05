6662ae0d40e362c3b4fb50ed76f7333df9f4ac34
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:25:56 +0200

    add extension query 2

---
+++ extension_criteria/2_query.sql
@@ -0,0 +1,5 @@
+SELECT
+  SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END) AS Male_Doctorates_Percent,
+  SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END) AS Female_Doctorates_Percent
+FROM
+  `bigquery-public-data.ml_datasets.census_adult_income`
