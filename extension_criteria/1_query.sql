c798fa6257e0b56ea214a79fd6ebd2f163f3e3c6
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:19:10 +0200

    change advanced query 1 bc i misunderstood

--- extension_criteria/1_query.sql
+++ extension_criteria/1_query.sql
@@ -1,9 +1,5 @@
 SELECT
-    TRIM(sex) as sex,
-    COUNT(*) AS total_population,
-    COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) AS doctorates,
-    ROUND(COUNT(CASE WHEN TRIM(education) = 'Doctorate' THEN 1 END) * 100.0 / COUNT(*), 2) AS doctorates_percentage
+  COUNT(CASE WHEN TRIM(sex) = "Male" THEN 1 END) AS TotMales,
+  COUNT(CASE WHEN TRIM(sex) = "Female" THEN 1 END) AS TotFemales
 FROM
-    `bigquery-public-data.ml_datasets.census_adult_income`
-GROUP BY
-    sex;
+  `bigquery-public-data.ml_datasets.census_adult_income`
