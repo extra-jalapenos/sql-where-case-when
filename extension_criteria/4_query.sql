cd5cf58b267efda12402733bb969e6281b295989
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-05 19:41:18 +0200

    add comment to explain solution

---
+++ extension_criteria/4_query.sql
@@ -0,0 +1,9 @@
+SELECT
+	SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END) AS Male_Doctorates_Percent,
+	SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) * 100.0 / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END) AS Female_Doctorates_Percent,
+	ROUND((SUM(CASE WHEN TRIM(sex) = 'Male' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Male' THEN 1 ELSE 0 END))/(SUM(CASE WHEN TRIM(sex) = 'Female' AND TRIM(education) = 'Doctorate' THEN 1 ELSE 0 END) / SUM(CASE WHEN TRIM(sex) = 'Female' THEN 1 ELSE 0 END)), 2) AS FemMalDocRatio
+FROM
+	`bigquery-public-data.ml_datasets.census_adult_income`
+WHERE TRIM(native_country) = "Italy"
+
+-- this makes sense because WHERE is evaluated before SELECT in execution
